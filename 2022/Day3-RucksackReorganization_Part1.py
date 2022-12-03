d45a3e2213a11f956e3ba89b6848c27957586185
KevZ3742 <66284340+KevZ3742@users.noreply.github.com>
2022-12-03 00:13:33 -0600

    Add files via upload

---
+++ 2022/Day3-RucksackReorganization_Part1.py
@@ -0,0 +1,22 @@
+import string
+
+values = dict()
+for index, letter in enumerate(string.ascii_letters):
+    values[letter] = index + 1
+
+sum = 0
+sack = ["", ""]
+
+with open('RucksackReorganization.txt') as file:
+    for line in file:
+        line = line.split('\n')
+
+        sack[0] = line[0][:int(len(line[0])/2)]
+        sack[1] = line[0][int(len(line[0])/2):len(line[0])]
+
+        for var in range(len(sack[0])):
+            if(sack[0][var] in sack[1]):
+                sum = sum + values[sack[0][var]]
+                break
+        
+print(sum)
