370006e631f7cce50442f892acf0550846ae8465
KevZ3742 <kzhang3742@gmail.com>
2022-12-04 01:10:21 -0600

    day 4

---
+++ 2022/Day4-CampCleanup_Part1.py
@@ -0,0 +1,18 @@
+import re
+
+total = 0
+count = 0
+
+with open('Day4-CampCleanup.txt') as file:
+    for line in file:
+        count = count + 1
+        line = line.split('\n')
+        line = re.split('-|,', line[0])
+        line = [int(a) for a in line]
+
+        if(all(e in range(line[0], line[1]+1) for e in range(line[2], line[3]+1))):
+            total = total + 1
+        elif(all(e in range(line[2], line[3]+1) for e in range(line[0], line[1]+1))):
+            total = total + 1
+
+print(total)
---
+++ 2022/Day4-CampCleanup_Part2.py
@@ -0,0 +1,18 @@
+import re
+
+total = 0
+count = 0
+
+with open('Day4-CampCleanup.txt') as file:
+    for line in file:
+        count = count + 1
+        line = line.split('\n')
+        line = re.split('-|,', line[0])
+        line = [int(a) for a in line]
+
+        if(line[0] in range(line[2], line[3]+1) or line[1] in range(line[2], line[3]+1)):
+            total = total + 1
+        elif(line[2] in range(line[0], line[1]+1) or line[2] in range(line[0], line[1]+1)):
+            total = total + 1
+
+print(total)
